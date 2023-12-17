a7222b48def3ee78e633e9a35e501fbb8dbf3f4f
RaviHelon-msf <ravihelon@gmail.com>
2023-12-17 18:14:52 -0300

    feat: add boilerplate
    Added boilerplate where code can start being added

---
+++ dev/Class1.cs
@@ -0,0 +1,15 @@
+﻿// WebCrawler.cs
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public static class WebCrawler
+{
+    // Write a Sum function
+
+    // Create a Struct that can save a Result named WebCrawlerResult
+
+    //create CrawlWebsiteAsync to do the get request
+
+}
