15afe6159aa6d9901539ef310f2721932d3302b2
RaviHelon-msf <ravihelon@gmail.com>
2023-12-21 23:49:52 -0300

    feat: update dotnet version

---
+++ dev/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public static class WebCrawler
+{
+    // Write a Sum function
+
+    public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    // Create a Struct that can save a Result named WebCrawlerResult
+
+    //create CrawlWebsiteAsync to do the get request
+
+}
--- dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
+++ dev/obj/Debug/net8.0/dev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7222b48def3ee78e633e9a35e501fbb8dbf3f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("dev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
