901f607563a7c17b64c225e3fe0eae407cf6ac6e
RaviHelon-msf <ravihelon@gmail.com>
2023-12-24 16:14:03 -0300

    ci/cd: little fixes

--- dev/Program.cs
+++ dev/Program.cs
@@ -7,11 +7,6 @@ public static class WebCrawler
 {
     // Write a Sum function
 
-    public static int Sum(int a, int b)
-        {
-            return a + b;
-        }
-
     // Create a Struct that can save a Result named WebCrawlerResult
 
     //create CrawlWebsiteAsync to do the get request
